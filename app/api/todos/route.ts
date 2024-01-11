b8bd7974a5cb4f84a423501ece9b59abcc38d0e5
Wilson <wilsontestonijr@gmail.com>
2024-01-10 23:59:08 -0300

    A7.2-2 - Migrando o nosso GET e POST de Todos

---
+++ app/api/todos/route.ts
@@ -0,0 +1,38 @@
+import { todoController } from "@server/controller/todo";
+
+export async function GET(request: Request) {
+  return await todoController.get(request);
+}
+
+export async function POST(request: Request) {
+  return await todoController.create(request);
+}
+
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { todoController } from "@server/controller/todo";
+
+// export default function GET(request: Request) {
+//   const response = await todoController.get(request, response);
+//   return Response.json({ response });
+// }
+
+// export default async function handler(
+//   request: NextApiRequest,
+//   response: NextApiResponse
+// ) {
+//   if (request.method === "GET") {
+//     await todoController.get(request, response);
+//     return;
+//   }
+
+//   if (request.method === "POST") {
+//     await todoController.create(request, response);
+//     return;
+//   }
+
+//   response.status(405).json({
+//     error: {
+//       message: "Method not allowed",
+//     },
+//   });
+// }
